3807f0d8aeba7061d915fec02abd4f5fc5368f6f
Priyanshu1981 <priyanshu.technophile@gmail.com>
2026-07-05 23:22:38 +0530

    Add files via upload

---
+++ Week1/JUnit, Mockito and SL4J/JUnitSetupExercise/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
