1957e7c8259a47a603e4a556044d78d9bc96541c
Priyanshu1981 <priyanshu.technophile@gmail.com>
2026-07-27 16:09:35 +0530

    Add files via upload

---
+++ Week 5/react apps/01-myfirstreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Hands-on 1: Set up a React environment using create-react-app and print
+// a welcome heading.
+function App() {
+  return (
+    <div>
+      <h1>welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
